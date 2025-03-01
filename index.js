f1236d7cbb3937a06a6f89e0dbaaae95c2a3e932
IrekIan <ir.jn.kr@gmail.com>
2025-03-01 18:25:13 +0000

    if added
    as above

--- index.js
+++ index.js
@@ -17,32 +17,37 @@ let pTok = document.querySelector("#weight-output-p-to-k");
 btn.addEventListener("click", function () {
   // LENGTH
   let value = document.querySelector("#value-input").value;
-  function meterToFeet() {
-    mTof.innerHTML = `${value} meters = ${value * 3.28} feet`;
-  }
-  meterToFeet(value);
-  function feetToMeter() {
-    fTom.innerHTML = `${value} feet = ${value * 0.3} meters`;
-  }
-  feetToMeter(value);
-  // VOLUME
-  function litersToGallons() {
-    lTog.innerHTML = `${value} liters = ${value * 0.26} gallons`;
-  }
-  litersToGallons(value);
+  if (value < 1) {
+    alert("Please enter a value");
+    return;
+  } else {
+    function meterToFeet() {
+      mTof.innerHTML = `${value} meters = ${value * 3.28} feet`;
+    }
+    meterToFeet(value);
+    function feetToMeter() {
+      fTom.innerHTML = `${value} feet = ${value * 0.3} meters`;
+    }
+    feetToMeter(value);
+    // VOLUME
+    function litersToGallons() {
+      lTog.innerHTML = `${value} liters = ${value * 0.26} gallons`;
+    }
+    litersToGallons(value);
 
-  function gallonsToLiters() {
-    gTol.innerHTML = `${value} gallons = ${value * 0.26} liters`;
-  }
-  gallonsToLiters(value);
-  // WEIGHT
-  function kilogramsToPounds() {
-    kTop.innerHTML = `${value} kilograms = ${value * 2.2} pounds`;
-  }
-  kilogramsToPounds(value);
+    function gallonsToLiters() {
+      gTol.innerHTML = `${value} gallons = ${value * 0.26} liters`;
+    }
+    gallonsToLiters(value);
+    // WEIGHT
+    function kilogramsToPounds() {
+      kTop.innerHTML = `${value} kilograms = ${value * 2.2} pounds`;
+    }
+    kilogramsToPounds(value);
 
-  function poundsToKilograms() {
-    pTok.innerHTML = `${value} pounds = ${value * 0.45} kilograms`;
+    function poundsToKilograms() {
+      pTok.innerHTML = `${value} pounds = ${value * 0.45} kilograms`;
+    }
+    poundsToKilograms(value);
   }
-  poundsToKilograms(value);
 });
